ea409e0e4db143d570dcf6582600f5991da7aa9a
christiano <christiano.pulsar@gmail.com>
2021-02-10 22:35:53 +0000

    Add user controller

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pulsar.AlphacA.Users;
+
+namespace Pulsar.AlphacA.Controllers
+{
+  [ApiController]
+  [Route("user")]
+  public class UserController : ControllerBase
+  {
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<User> Get()
+    {
+      return new User[]
+      {
+        new User
+        {
+          UserName = "roger.waters",
+          FirstName = "Roger",
+          LastName = "Waters"
+        }
+      };
+    }
+  }
+}
--- domain/Users/User.cs
+++ domain/Users/User.cs
@@ -3,5 +3,7 @@ namespace Pulsar.AlphacA.Users
   public class User
   {
     public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
   }
 }
