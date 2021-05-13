98d6c71454289a06764821304a51682a1c518aa3
christiano <christiano.pulsar@gmail.com>
2021-05-13 20:59:40 +0000

    cleanup

--- tests/Interpolate.Tests.cs
+++ tests/Interpolate.Tests.cs
@@ -1,54 +1,67 @@
-
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
-public class InterpolateTests
+namespace Interpolation
 {
-  [Fact]
-  public void NullValueThrowsException()
+  public class InterpolateTests
   {
-    var ex = Assert.Throws<ArgumentException>(() => (null as string).Interpolate(new Dictionary<string, string> { { "name", "test" } }));
-    Assert.Equal("Argument <value> cannot be null or empty", ex.Message);
-  }
+    [Fact]
+    public void NullInputThrowsException()
+    {
+      var ex = Assert.Throws<ArgumentException>(() => (null as string).Interpolate(new Dictionary<string, string> { { "name", "test" } }));
+      Assert.Equal("Argument <input> cannot be null or empty", ex.Message);
+    }
 
-  [Fact]
-  public void EmptyValueThrowsException()
-  {
-    var ex = Assert.Throws<ArgumentException>(() => "".Interpolate(new Dictionary<string, string> { { "name", "test" } }));
-    Assert.Equal("Argument <value> cannot be null or empty", ex.Message);
-  }
+    [Fact]
+    public void EmptyInputThrowsException()
+    {
+      var ex = Assert.Throws<ArgumentException>(() => "".Interpolate(new Dictionary<string, string> { { "name", "test" } }));
+      Assert.Equal("Argument <input> cannot be null or empty", ex.Message);
+    }
 
-  [Fact]
-  public void NullTokensThrowsException()
-  {
-    var ex = Assert.Throws<ArgumentException>(() => "Some value".Interpolate(null));
-    Assert.Equal("Argument <tokens> cannot be null or empty", ex.Message);
-  }
+    [Fact]
+    public void NullTokensThrowsException()
+    {
+      var ex = Assert.Throws<ArgumentException>(() => "Some value".Interpolate(null));
+      Assert.Equal("Argument <tokens> cannot be null or empty", ex.Message);
+    }
 
-  [Fact]
-  public void EmptyTokensThrowsException()
-  {
-    var ex = Assert.Throws<ArgumentException>(() => "Some value".Interpolate(new Dictionary<string, string>()));
-    Assert.Equal("Argument <tokens> cannot be null or empty", ex.Message);
-  }
+    [Fact]
+    public void EmptyTokensThrowsException()
+    {
+      var ex = Assert.Throws<ArgumentException>(() => "Some value".Interpolate(new Dictionary<string, string>()));
+      Assert.Equal("Argument <tokens> cannot be null or empty", ex.Message);
+    }
 
-  [Fact]
-  public void NoMatchReturnsUnmodifiedString()
-  {
-    Assert.Equal("Some value", "Some value".Interpolate(new Dictionary<string, string>{{"p", "x"}}));
-  }
+    [Fact]
+    public void NoMatchReturnsUnmodifiedString()
+    {
+      Assert.Equal("Some value", "Some value".Interpolate(new Dictionary<string, string> { { "p", "x" } }));
+    }
 
-  [Fact]
-  public void SingleMatchIsValid()
-  {
-    Assert.Equal("Hello Jim", "Hello [name]".Interpolate(new Dictionary<string, string>{{"name", "Jim"}}));
-  }
+    [Fact]
+    public void SingleMatchIsResolved()
+    {
+      Assert.Equal("Hello Jim", "Hello [name]".Interpolate(new Dictionary<string, string> { { "name", "Jim" } }));
+    }
 
-  [Fact]
-  public void EscapedStringIsIgnored()
-  {
-    Assert.Equal("Hello Jim [author]", "Hello [name] [[author]]".Interpolate(new Dictionary<string, string>{{"name", "Jim"}}));
+    [Fact]
+    public void MultipleMatchesAreResolved()
+    {
+      Assert.Equal("Hello Jim. Jim is here", "Hello [name]. [name] is here".Interpolate(new Dictionary<string, string> { { "name", "Jim" } }));
+    }
+
+    [Fact]
+    public void SingleMatchWithEscapeIsResolved()
+    {
+      Assert.Equal("Hello [Jim]", "Hello [[name]]".Interpolate(new Dictionary<string, string> { { "name", "Jim" } }));
+    }
+
+    [Fact]
+    public void EscapedStringIsIgnored()
+    {
+      Assert.Equal("Hello Jim [author]", "Hello [name] [[author]]".Interpolate(new Dictionary<string, string> { { "name", "Jim" } }));
+    }
   }
 }
--- tests/Interpolate.cs
+++ tests/Interpolate.cs
@@ -1,45 +1,31 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 
-public static class StringExtensions
+namespace Interpolation
 {
-  public static string Interpolate([NotNull] this string value, IDictionary<string, string> tokens)
+  public static class StringExtensions
   {
-    if (string.IsNullOrWhiteSpace(value))
+    public static string Interpolate(this string input, IDictionary<string, string> tokens)
     {
-      throw new ArgumentException($"Argument <{nameof(value)}> cannot be null or empty");
-    }
-
-    if (tokens == null || tokens.Count == 0)
-    {
-      throw new ArgumentException($"Argument <{nameof(tokens)}> cannot be null or empty");
-    }
-
-    string result = value;
-
-    //tokens.Aggregate()
-
-    foreach (var token in tokens)
-    {
-      if (string.IsNullOrWhiteSpace(token.Key))
+      if (string.IsNullOrWhiteSpace(input))
       {
-        throw new InvalidOperationException("");
+        throw new ArgumentException($"Argument <{nameof(input)}> cannot be null or empty");
       }
 
-      // TODO: handle token not found in string
+      if (tokens == null || tokens.Count == 0)
+      {
+        throw new ArgumentException($"Argument <{nameof(tokens)}> cannot be null or empty");
+      }
 
-      result = result.Replace($"[{token.Key}]", token.Value);
-      // {1} 
-      //var pattern = @"\[guitarist\]";
+      string result = input;
 
-      //result = Regex.Replace(result, @$"\[{x.Key}\]", x.Value);
-      //result = Regex.Replace(value, pattern, x.Value);
+      foreach (var token in tokens)
+      {
+        result = result.Replace($"[{token.Key}]", token.Value);
+      }
+
+      return result.Replace("[[", "[").Replace("]]", "]");
     }
-
-    return result.Replace("[[", "[").Replace("]]", "]");
   }
 }
